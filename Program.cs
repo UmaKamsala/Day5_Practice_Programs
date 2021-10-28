859504ad51e2eb3b000ceb2db9dcb14581df4642
UmaKamsala <kamsalauma@gmail.com>
2021-10-28 19:53:19 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day5_Practice_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Flipcoin Program");
+        }
+    }
+}
